90bd62907bca6338799cc739526a6d05566d42b7
Luke Mastrocola <lukejmastrocola@gmail.com>
2025-06-25 13:21:37 +1000

    Card now renders weather station data when marker is clicked on the map

--- src/app/GoogleMaps.tsx
+++ src/app/GoogleMaps.tsx
@@ -1,6 +1,14 @@
-import { Box, Card, CardContent, Typography, CardHeader } from "@mui/material";
-import { useEffect, useRef } from "react";
-import { WeatherStations } from "./types";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CircularProgress,
+  CardHeader,
+} from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { WeatherStations, Measurements } from "./types";
+import { getStation } from "@/services/api";
 import { CellTower, LocationOn } from "@mui/icons-material";
 const GoogleMaps = ({
   weatherStations,
@@ -8,7 +16,18 @@ const GoogleMaps = ({
   weatherStations: WeatherStations[];
 }) => {
   const mapRef = useRef(null);
+  const [measurements, setMeasurements] = useState<Measurements[]>([]);
+  const [loading] = useState(false);
+  const [selectedStation, setSelectedStation] =
+    useState<WeatherStations | null>(null);
 
+  const StationDetails = selectedStation && [
+    { key: "Site: ", value: selectedStation.site },
+    { key: "Portfolio: ", value: selectedStation.portfolio },
+    { key: "State: ", value: selectedStation.state },
+    { key: "Longitude: ", value: selectedStation.longitude },
+    { key: "Latitude: ", value: selectedStation.latitude },
+  ];
   useEffect(() => {
     if (mapRef.current) {
       const map = new window.google.maps.Map(mapRef.current, {
@@ -23,6 +42,15 @@ const GoogleMaps = ({
             lat: Number(station.latitude),
             lng: Number(station.longitude),
           },
+        });
+        marker.addListener("click", async () => {
+          setSelectedStation(station);
+          try {
+            setMeasurements(await getStation(station.id));
+          } catch (error) {
+            console.error("Error fetching measurements:", error);
+            setMeasurements([]);
+          }
         });
       });
     }
@@ -43,34 +71,64 @@ const GoogleMaps = ({
           subheader={"Showing X weather stations"}
           avatar={<CellTower />}
         />
-
-        <CardContent sx={{ flexGrow: 1 }}>
-          <Box
-            sx={{
-              display: "flex",
-              height: "100%",
-              p: 0,
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <LocationOn sx={{ fontSize: 96, color: "silver" }} />
-            <Typography sx={{ fontSize: 22, color: "silver" }}>
-              No station selected
-            </Typography>
-            <Typography
-              sx={{
-                color: "silver",
-                fontSize: 14,
-                textAlign: "center",
-                mb: 8,
-              }}
-            >
-              Click a marker on the map for station details
-            </Typography>
-          </Box>
-        </CardContent>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <CardContent sx={{ flexGrow: 1 }}>
+            {selectedStation ? (
+              <>
+                {StationDetails?.map((details, index) => (
+                  <Typography key={index}>
+                    {details.key}
+                    {details.value}
+                  </Typography>
+                ))}
+                {measurements.length ? (
+                  measurements.map((measurement, index) => (
+                    <Box key={index}>
+                      <Typography>
+                        {measurement.variable_name}
+                        {": "}
+                        {measurement.value}
+                        <br />
+                      </Typography>
+                    </Box>
+                  ))
+                ) : (
+                  <Typography variant="body2" color="text.secondary">
+                    No measurements available
+                  </Typography>
+                )}
+              </>
+            ) : (
+              <Box
+                sx={{
+                  display: "flex",
+                  height: "100%",
+                  p: 0,
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <LocationOn sx={{ fontSize: 96, color: "silver" }} />
+                <Typography sx={{ fontSize: 22, color: "silver" }}>
+                  No station selected
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "silver",
+                    fontSize: 14,
+                    textAlign: "center",
+                    mb: 8,
+                  }}
+                >
+                  Click a marker on the map for station details
+                </Typography>
+              </Box>
+            )}
+          </CardContent>
+        )}
       </Card>
     </Box>
   );
